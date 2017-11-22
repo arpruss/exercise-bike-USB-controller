8341286e955e6d5a9277795cfa2b1b2b70d77d42
arpruss <arpruss@gmail.com>
2017-11-22 09:09:14 -0600

    rename

--- ExerciseController.ino
+++ ExerciseController.ino
@@ -1,102 +1,246 @@
+  // gamecube controller adapter
+// This requires this branch of the Arduino STM32 board: 
+//    https://github.com/arpruss/Arduino_STM32/tree/addMidiHID
+// Software steps:
+//    Install this bootloader: https://github.com/rogerclarkmelbourne/STM32duino-bootloader/blob/master/binaries/generic_boot20_pb12.bin?raw=true
+//    Instructions here: http://wiki.stm32duino.com/index.php?title=Burning_the_bootloader#Flashing_the_bootloader_onto_the_Black_Pill_via_UART_using_a_Windows_machine/  
+//    Install official Arduino Zero board
+//    Put the contents of the above branch in your Arduino/Hardware folder
+//    If on Windows, run drivers\win\install_drivers.bat
+// Note: You may need to adjust permissions on some of the dll, exe and bat files.
+
+// Facing GameCube socket (as on console), flat on top:
+//    123
+//    ===
+//    456
+
+// Connections for GameCube adapter:
+// GameCube 2--PA6
+// GameCube 2--1Kohm--3.3V
+// GameCube 3--GND
+// GameCube 4--GND
+// GameCube 6--3.3V
+// Put a 10 uF and 0.1 uF capacitor between 3.3V and GND right on the black pill board.
+// optional: connect GameCube 1--5V (rumble, make sure there is enough current)
+
+// Put LEDs + resistors (100-220 ohm) between PA0,PA1,PA2,PA3 and 3.3V
+// Put momentary pushbuttons between PA4 (decrement),PA5 (increment) and 3.3V
+
+// Connections for elliptical/bike:
+// GND--GND
+// PA7--4.7Kohm--rotation detector [the resistor may not be needed]
+// PA7--100Kohm--3.3V
+
+// For direction switch, to tell the device if you're pedaling backwards or forwards
+// PA8 - direction control switch, one side
+// 3.3V - direction control switch, other side
+
+// Connections for Nunchuck
+// GND--GND
+// 3.3V--3.3V
+// PB6--10Kohm--3.3V
+// PB7--10Kohm--3.3V
+// PB6--SCL
+// PB7--SDA
+
+
+
+// Make sure there are 4.7Kohm? pullups for the two i2c data lines
+
+#include <libmaple/iwdg.h>
+#include <libmaple/usb_cdcacm.h>
+#include <libmaple/usb.h>
+#include "debounce.h"
 #include "gamecube.h"
-#include "dwt.h"
 
-static const uint8_t maxFails = 4;
-static uint8_t fails;
+Debounce debounceDown(downButton, HIGH);
+Debounce debounceUp(upButton, HIGH);
 
-const uint32_t cyclesPerUS = (SystemCoreClock / 1000000ul);
-const uint32_t quarterBitSendingCycles = cyclesPerUS * 5 / 4;
-const uint32_t bitReceiveCycles = cyclesPerUS * 4;
-const uint32_t halfBitReceiveCycles = cyclesPerUS * 2;
-
-void gameCubeInit(void) {
-  gpio_set_mode(gcPort, gcPin, GPIO_OUTPUT_OD); // set open drain output
-  CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
-  DWT->CTRL |= 1;
-  fails = maxFails; // force update
+void displayNumber(uint8_t x) {
+  for (int i=0; i<numIndicators; i++, x>>=1) 
+    analogWrite(indicatorLEDs[i], (x&1) ? (255-ledBrightnessLevels[i]) : 255);
+    //digitalWrite(indicatorLEDs[i], !(x&1));
 }
 
-// at most 32 bits can be sent
-void gameCubeSendBits(uint32_t data, uint8_t bits) {
-  data <<= 32 - bits;
-  DWT->CYCCNT = 0;
-  uint32_t timerEnd = DWT->CYCCNT;
-  do {
-    gpio_write_bit(gcPort, gcPin, 0);
-    if (0x80000000ul & data)
-      timerEnd += quarterBitSendingCycles;
-    else
-      timerEnd += 3 * quarterBitSendingCycles;
-    while (DWT->CYCCNT < timerEnd) ;
-    gpio_write_bit(gcPort, gcPin, 1);
-    if (0x80000000ul & data)
-      timerEnd += 3 * quarterBitSendingCycles;
-    else
-      timerEnd += quarterBitSendingCycles;
-    data <<= 1;
-    bits--;
-    while (DWT->CYCCNT < timerEnd) ;
-  } while (bits);
+void updateDisplay() {
+  displayNumber(numInjectionModes >= 16 ? injectionMode : injectionMode+1);
 }
 
-// bits must be greater than 0
-uint8_t gameCubeReceiveBits(void* data0, uint32_t bits) {
-  uint8_t* data = (uint8_t*)data0;
+void setup() {
+  for (int i=0; i<numIndicators; i++)
+    pinMode(indicatorLEDs[i], OUTPUT);
+  pinMode(downButton, INPUT_PULLDOWN);
+  pinMode(upButton, INPUT_PULLDOWN);
+  
+#ifdef SERIAL_DEBUG
+  Serial.begin(115200);
+  displayNumber(3);
+  delay(4000);
+  Serial.println("gamecube controller adapter");
+#else
+  Joystick.setManualReportMode(true);
+#endif
 
-  uint32_t timeout = bitReceiveCycles * bits / 2 + 4;
+  ellipticalInit();
 
-  uint8_t bitmap = 0x80;
+#ifdef ENABLE_GAMECUBE
+  gameCubeInit();
+#endif
+#ifdef ENABLE_NUNCHUCK
+  nunchuckInit();
+#endif
 
-  *data = 0;
-  do {
-    if (!gpio_read_bit(gcPort, gcPin)) {
-      DWT->CYCCNT = 0;
-      while (DWT->CYCCNT < halfBitReceiveCycles - 2) ;
-      if (gpio_read_bit(gcPort, gcPin)) {
-        *data |= bitmap;
-      }
-      bitmap >>= 1;
-      bits--;
-      if (bitmap == 0) {
-        data++;
-        bitmap = 0x80;
-        if (bits)
-          *data = 0;
-      }
-      while (!gpio_read_bit(gcPort, gcPin) && --timeout) ;
-      if (timeout == 0) {
-        break;
-      }
-    }
-  } while (--timeout && bits);
+  debounceDown.begin();
+  debounceUp.begin();
 
-  return bits == 0;
+  pinMode(ledPinID, OUTPUT);
+
+  EEPROM8_init();
+  int i = EEPROM8_getValue(EEPROM_VARIABLE_INJECTION_MODE);
+  if (i < 0)
+    injectionMode = 0;
+  else
+    injectionMode = i; 
+
+  if (injectionMode > numInjectionModes)
+    injectionMode = 0;
+
+  savedInjectionMode = injectionMode;
+  
+  updateDisplay();
+
+  lastChangedModeTime = 0;
+  iwdg_init(IWDG_PRE_256, watchdogSeconds*156);
 }
 
-uint8_t gameCubeReceiveReport(GameCubeData_t* data, uint8_t rumble) {
-  if (fails >= maxFails) {
-    nvic_globalirq_disable();
-    gameCubeSendBits(0b000000001l, 9);
-    nvic_globalirq_enable();
-    delayMicroseconds(400);
-    fails = 0;
-  }
-  nvic_globalirq_disable();
-  gameCubeSendBits(rumble ? 0b0100000000000011000000011l : 0b0100000000000011000000001l, 25);
-  uint8_t success = gameCubeReceiveBits(data, 64);
-  data->device = DEVICE_GAMECUBE;
-  nvic_globalirq_enable();
-  if (success && 0 == (data->buttons & 0x80) && (data->buttons & 0x8000) ) {
-    return 1;
+//uint8_t poorManPWM = 0;
+void updateLED(void) {
+  if (((validDevice != DEVICE_NONE) ^ ellipticalRotationDetector) && validUSB) {
+        gpio_write_bit(ledPort, ledPin, 0); //poorManPWM);
+    //poorManPWM ^= 1;
   }
   else {
-    fails++;
-    return 0;
+    gpio_write_bit(ledPort, ledPin, 1);
+    //analogWrite(ledPinID, 255);
   }
+  //gpio_write_bit(ledPort, ledPin, ! (((validDevice != DEVICE_NONE) ^ ellipticalRotationDetector) && validUSB));
 }
 
-uint8_t gameCubeReceiveReport(GameCubeData_t* data) {
-  return gameCubeReceiveReport(data, 0);
+uint8_t receiveReport(GameCubeData_t* data) {
+  uint8_t success;
+
+#ifdef ENABLE_GAMECUBE
+  if (validDevice == DEVICE_GAMECUBE || validDevice == DEVICE_NONE) {
+    success = gameCubeReceiveReport(data);
+    if (success) {
+      validDevice = DEVICE_GAMECUBE;
+      return 1;
+    }
+    validDevice = DEVICE_NONE;
+  }
+#endif
+#ifdef ENABLE_NUNCHUCK
+  if (validDevice == DEVICE_NUNCHUCK || nunchuckDeviceInit()) {
+    success = nunchuckReceiveReport(data);
+    if (success) {
+      validDevice = DEVICE_NUNCHUCK;
+      return 1;
+    }
+  }
+#endif
+  validDevice = DEVICE_NONE;
+
+  data->joystickX = 128;
+  data->joystickY = 128;
+  data->cX = 128;
+  data->cY = 128;
+  data->buttons = 0;
+  data->shoulderLeft = 0;
+  data->shoulderRight = 0;
+  data->device = DEVICE_NONE;
+
+  return 0;
 }
 
+void loop() {
+  GameCubeData_t data;
+  EllipticalData_t elliptical;
+
+  iwdg_feed();
+  
+  uint32_t t0 = millis();
+  while (debounceDown.getRawState() && debounceUp.getRawState() && (millis()-t0)<5000)
+        updateLED();
+  
+  iwdg_feed();
+
+  if ((millis()-t0)>=5000) {
+    displayNumber(0xF);
+    injectionMode = 0;
+    EEPROM8_reset();
+    updateDisplay();
+    savedInjectionMode = 0;
+  }
+  else {
+    t0 = millis();
+    do {
+      if (debounceDown.wasReleased()) {
+        if (injectionMode == 0)
+          injectionMode = numInjectionModes-1;
+        else
+          injectionMode--;
+        lastChangedModeTime = millis();
+        updateDisplay();
+      }
+      
+      if (debounceUp.wasPressed()) {
+        injectionMode = (injectionMode+1) % numInjectionModes;
+        lastChangedModeTime = millis();
+        updateDisplay();
+  #ifdef SERIAL_DEBUG      
+        Serial.println("Changed to "+String(injectionMode));
+  #endif
+      }
+      updateLED();
+    } while((millis()-t0) < 6);
+  }
+
+  ellipticalUpdate(&elliptical);
+      
+  if (savedInjectionMode != injectionMode && (millis()-lastChangedModeTime) >= saveInjectionModeAfterMillis) {
+#ifdef SERIAL_DEBUG
+    Serial.println("Need to store");
+#endif
+    EEPROM8_storeValue(EEPROM_VARIABLE_INJECTION_MODE, injectionMode);
+    savedInjectionMode = injectionMode;
+  }
+
+#ifndef SERIAL_DEBUG
+  if (!usb_is_connected(USBLIB) || !usb_is_configured(USBLIB)) {
+    // we're disconnected; save power by not talking to controller
+    validUSB = 0;
+    updateLED();
+    return;
+  } // TODO: fix library so it doesn't send on a disconnected connection; currently, we're relying on the watchdog reset 
+  else {
+    validUSB = 1;
+  }
+    // if a disconnection happens at the wrong time
+#else
+  validUSB = 1;
+#endif
+
+  receiveReport(&data);
+#ifdef SERIAL_DEBUG
+//  Serial.println("buttons1 = "+String(data.buttons));  
+//  Serial.println("joystick = "+String(data.joystickX)+","+String(data.joystickY));  
+//  Serial.println("c-stick = "+String(data.cX)+","+String(data.cY));  
+//  Serial.println("shoulders = "+String(data.shoulderLeft)+","+String(data.shoulderRight));      
+#else
+if (usb_is_connected(USBLIB) && usb_is_configured(USBLIB)) 
+    inject(injectors + injectionMode, &data, &elliptical);
+#endif
+    
+  updateLED();
+}
 
